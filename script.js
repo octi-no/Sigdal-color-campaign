3a80a4988b4253f782783654488533602f855f12
Rolf Hammerstad <epostenmin@gmail.com>
2019-09-26 09:03:21 +0200

    Updated styles, removed interactivity

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ var handleLightness = document.getElementById('handle-lightness');
 
 var target = document.getElementById('target');
 
-pickerHue.addEventListener('click', function(event) {
-	hue = range(event.layerY, sliderHeight, 360);
-	update();
-});
-pickerSaturation.addEventListener('click', function(event) {
-	saturation = range(event.layerY, sliderHeight, 100);
-	update();
-});
-pickerLightness.addEventListener('click', function(event) {
-	lightness = range(event.layerY, sliderHeight, 100);
-	update();
-});
+// pickerHue.addEventListener('click', function(event) {
+// 	hue = range(event.layerY, sliderHeight, 360);
+// 	update();
+// });
+// pickerSaturation.addEventListener('click', function(event) {
+// 	saturation = range(event.layerY, sliderHeight, 100);
+// 	update();
+// });
+// pickerLightness.addEventListener('click', function(event) {
+// 	lightness = range(event.layerY, sliderHeight, 100);
+// 	update();
+// });
 
 function update() {
 	target.style.fill =
@@ -45,24 +45,33 @@ update();
 
 var animations = [
 	{
-		hue: 32,
-		saturation: 14,
-		lightness: 56
+		hue: 0,
+		saturation: 0,
+		lightness: 38
 	},
 	{
-		hue: 187,
-		saturation: 27,
-		lightness: 56
+		hue: 139,
+		saturation: 13,
+		lightness: 75
 	},
 	{
-		hue: 327,
-		saturation: 25,
-		lightness: 77
+		hue: 207,
+		saturation: 5,
+		lightness: 62
+	},
+	{
+		hue: 13,
+		saturation: 5,
+		lightness: 64
+	},
+	{
+		hue: 45,
+		saturation: 61,
+		lightness: 88
 	}
 ];
 animationIndex = 0;
 var animationInterval = setInterval(function() {
-	console.log('run run run', animationIndex);
 	if (animationIndex == animations.length) {
 		animationIndex = 0;
 	}
@@ -71,8 +80,41 @@ var animationInterval = setInterval(function() {
 	lightness = animations[animationIndex].lightness;
 	update();
 	animationIndex++;
-}, 3000);
+}, 2000);
 
 document.addEventListener('click', function() {
 	clearTimeout(animationInterval);
 });
+
+// var mouseY = 0;
+// var mouseStartY = 0;
+// var mousedown = false;
+// var colorType = '';
+
+// document.addEventListener('mousemove', function(event) {
+// 	if (mousedown) {
+// 		var diff = event.y - 20;
+// 		mouseStartY = event.y;
+
+// 		if (colorType == 'picker-hue') {
+// 			hue += range(diff, 256, sliderHeight);
+// 		} else if (colorType == 'picker-saturation') {
+// 			saturation += range(diff, 100, sliderHeight);
+// 		} else {
+// 			lightness += range(diff, 100, sliderHeight);
+// 		}
+// 		update();
+// 	}
+// });
+
+// document.addEventListener('mousedown', function(event) {
+// 	if (event.target.classList.contains('picker')) {
+// 		mousedown = true;
+// 		mouseStartY = event.y;
+// 		colorType = event.target.id;
+// 	}
+// });
+
+// document.addEventListener('mouseup', function(event) {
+// 	mousedown = false;
+// });
